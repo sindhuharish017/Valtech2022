340c841b116b4e53e55d60a90477edfd6178393c
Sindhu H <Sindhu.H@valtech.com>
2022-09-30 16:15:02 +0530

    first maven project

---
+++ maven_example/src/main/java/com/valtech/traing/maven/example/App.java
@@ -0,0 +1,13 @@
+package com.valtech.traing.maven.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
